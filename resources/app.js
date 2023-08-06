0480db43a5fc38e84d94f4a0e08fda3d2ce010d1
spectre-xenon <62655598+spectre-xenon@users.noreply.github.com>
2023-08-07 00:14:52 +0300

    finished the rest of the page

--- resources/app.js
+++ resources/app.js
@@ -1,9 +1,21 @@
-function play() {
-  const audio = document.getElementById("spear-of-justise");
-  audio.play();
-  audio.volume = 0.55;
-  audio.loop = true;
+function play_1() {
+  const spear_of_justise = document.getElementById("spear-of-justise");
+  const spider_dance = document.getElementById("spider-dance");
+  spider_dance.pause();
+  spear_of_justise.play();
+  spear_of_justise.volume = 0.55;
+  spear_of_justise.loop = true;
 }
+
+function play_2() {
+  const spear_of_justise = document.getElementById("spear-of-justise");
+  const spider_dance = document.getElementById("spider-dance");
+  spear_of_justise.pause();
+  spider_dance.play();
+  spider_dance.volume = 0.55;
+  spider_dance.loop = true;
+}
+
 function hover(img) {
   img.src = "./resources/imgs/soul_broken.png";
 }
