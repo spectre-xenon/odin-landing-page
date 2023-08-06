9fcb3a37f430fe7df7a629246f700095fc543be9
spectre-xenon <62655598+spectre-xenon@users.noreply.github.com>
2023-08-06 03:31:09 +0300

    lowered music volume

--- resources/app.js
+++ resources/app.js
@@ -1,6 +1,7 @@
 function play() {
   const audio = document.getElementById("spear-of-justise");
   audio.play();
+  audio.volume = 0.55;
 }
 function hover(img) {
   img.src = "./resources/imgs/soul_broken.png";
