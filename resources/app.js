06e66e8fbc484c24963ff8c63538f92e780d2d55
spectre-xenon <62655598+spectre-xenon@users.noreply.github.com>
2023-08-06 03:04:45 +0300

    finished header section and added phone-blocker

---
+++ resources/app.js
@@ -0,0 +1,11 @@
+function play() {
+  const audio = document.getElementById("spear-of-justise");
+  audio.play();
+}
+function hover(img) {
+  img.src = "./resources/imgs/soul_broken.png";
+}
+
+function hoverOut(img) {
+  img.src = "./resources/imgs/soul.png";
+}
