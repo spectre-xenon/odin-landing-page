714ed0e5942943de43139b64589f57c574a29012
spectre-xenon <62655598+spectre-xenon@users.noreply.github.com>
2023-08-06 17:49:33 +0300

    header changes

--- resources/app.js
+++ resources/app.js
@@ -2,6 +2,7 @@ function play() {
   const audio = document.getElementById("spear-of-justise");
   audio.play();
   audio.volume = 0.55;
+  audio.loop = true;
 }
 function hover(img) {
   img.src = "./resources/imgs/soul_broken.png";
